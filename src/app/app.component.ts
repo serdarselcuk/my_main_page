97aab7153af57080a4c9eb02a51264e29967bf67
serdar selcuk <serdarselcu61@gmail.com>
2024-08-11 00:29:31 -0500

    moved into parameteres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'My Page';
+  salut = "Hello, dear visitor!"
+  definition = "I'm Serdar Selcuk, a developer at Chase. This page showcases my development skills, including Angular, Android, SpringBoot, Selenium, Appium, RestAssured, and more."
+  ending = "Explore the links to learn more about my projects and experience."
+
 }
