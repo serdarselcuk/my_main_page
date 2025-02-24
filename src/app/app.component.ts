4ad4b4ca9faed34bae7a221d4fdbd39abd907fee
serdar selcuk <serdarselcu61@gmail.com>
2025-02-24 01:47:51 -0600

    salut changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,9 @@ export class AppComponent {
 
   title = 'My Page';
   salut = "Hello, dear visitor!"
-  definition = "I'm a Software Developer with 5 years of experience building automation frameworks using Selenium, Appium, Espresso, and JDBC to test mobile, web, API, and database applications. I've had the opportunity to work across the entire automation landscape, thanks to my managers recognizing my eagerness to learn and tackle new challenges.  In addition to automation, I've also developed Android applications, including adding new features and fixing defects."
-  ending = "Explore the links to learn more about my projects and experience."
+  definition_1 = "I'm an Android Developer with a strong foundation in mobile development, specializing in building robust applications using Kotlin, Java, and Jetpack Compose. My experience includes optimizing UI performance, resolving critical production issues, and implementing shift-left testing strategies to enhance code quality and reduce defect resolution time."
+  definition_2 ="I've successfully migrated hybrid features to native Android components, resulting in a 20% improvement in page load times, and led initiatives to enhance mobile testing frameworks with Espresso, improving component-level testing and reducing reliance on end-to-end tests. I'm passionate about creating efficient, user-friendly mobile experiences and continuously improving development processes."
+  ending = "  Explore my projects and experience to see how I can contribute to your team."
   resume_link_text='my CV'
   resume_link='https://drive.google.com/file/d/1i1LAIuxRN7Pmbi-vUmTKNJRn0SxJq5w9/view?usp=sharing'
   resume_details='click to see my resume'
@@ -25,12 +26,12 @@ export class AppComponent {
   article_link_text='Article'
   article_link='https://drive.google.com/file/d/12cE0uCqdazwNtkzo966yZMYRIODoOxlu/view?usp=sharing'
   article_descirption='click to see the article'
-  md_thessis_link_text='MD thesis'
-  md_thessis_link= 'https://drive.google.com/file/d/1TUXckqIa2vQePE7l4MQ-4dGn_GBOSJJN/view?usp=sharing'
-  md_thessis_descirption='click to see the thessis'
-  md_publishment_link_text='MD publishment'
-  md_publishment_link='https://drive.google.com/file/d/1D3JVf1PIE5-13KJLi7mOAOsnPOCvyVWd/view?usp=sharing'
-  md_publishment_descirption='click to see publishment'
+  // md_thessis_link_text='MD thesis'
+  // md_thessis_link= 'https://drive.google.com/file/d/1TUXckqIa2vQePE7l4MQ-4dGn_GBOSJJN/view?usp=sharing'
+  // md_thessis_descirption='click to see the thessis'
+  // md_publishment_link_text='MD publishment'
+  // md_publishment_link='https://drive.google.com/file/d/1D3JVf1PIE5-13KJLi7mOAOsnPOCvyVWd/view?usp=sharing'
+  // md_publishment_descirption='click to see publishment'
   android_title='Android app - Math mind'
   android_link= 'https://github.com/serdarselcuk/mathMind_android'
   android_description="Click to see my Android application"
